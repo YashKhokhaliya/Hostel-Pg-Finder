4ba8a683ce1aa4343e3fd5623726fe0be608b980
YashK1 <yashkhokhaliya9@gmail.com>
2026-07-26 15:53:49 +0530

    Connection of Db is done

---
+++ backend/src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+const connectDB = async()=>{
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`)
+
+        console.log(`mongodb connection successfully !! DB host ${connectionInstance.connection.host}`)
+
+    }
+    catch(error){
+        console.log("mongodb connection error:-", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB
