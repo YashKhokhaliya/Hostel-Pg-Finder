535fda30eae07e34c78bcad01967c7350079cdf0
YashK1 <yashkhokhaliya9@gmail.com>
2026-08-06 18:15:11 +0530

    Remove unnecessary newline in database connection log and ensure proper formatting in user registration route

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose"
 const connectDB = async()=>{
     try{
         const connectionInstance = await mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`)
-
         console.log(`mongodb connection successfully !! DB host ${connectionInstance.connection.host}`)
 
     }
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -2,3 +2,11 @@ import { Router } from "express";
 import verifyJWT from "../middlewares/auth.middleware.js"
 import upload from "../middlewares/multer.middleware.js"
 
+import { userRegistration } from "../controllers/user.controller.js"
+
+const router = Router()
+
+router.route("/register").post(
+    upload.single('avatar'),
+    userRegistration
+)
