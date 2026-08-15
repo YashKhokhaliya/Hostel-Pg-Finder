3462df1c60851a3b6c9fa25936b1c37a7add77c8
Yash Kanzariya <yashkanzariya11@gmail.com>
2026-08-15 13:21:15 +0530

    Add rating model

---
+++ backend/src/models/rating.model.js
@@ -0,0 +1,30 @@
+import mongoose, {Schema} from "mongoose"
+
+const ratingSchema = new Schema(
+    {
+        user: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        hostel: {
+            type: mongoose.Types.ObjectId,
+            ref: 'Hostel',
+            required: true
+        },
+        rating: {
+            type: Number,
+            required: true,
+            min: 1,
+            max: 5
+        },
+    },
+    {timestamps: true}
+)
+
+ratingSchema.index(
+    {user: 1, hostel: 1},
+    {unique: true}
+)
+
+export const Rating = mongoose.model('Rating', ratingSchema)
